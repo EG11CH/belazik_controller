73444d4a304ce196d7c16e08ead4e930a5443912
EG11CH <slizevskijegor@gmail.com>
2024-10-21 17:13:45 +0300

    make urdf readable

--- launch.py
+++ launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
+from launch.actions import TimerAction
 
 def generate_launch_description():
     return LaunchDescription([
@@ -13,13 +14,16 @@ def generate_launch_description():
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': 'example_11/description/urdf/carlikebot.urdf.xarco'}],
+            parameters=[{'robot_description': Command(['xacro ', 'test.urdf'])}],
             output='screen',
         ),
-        Node(
+    TimerAction(
+        period=15.0,  
+        actions=[Node(
             package='controller_manager',
             executable='spawner',
             arguments=['rack_pinion_controller'],
             output='screen',
-        ),
-    ])
+        )],
+    ),
+ ])
